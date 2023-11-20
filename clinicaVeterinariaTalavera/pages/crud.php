51406abe078c28d8aaea923388ce9e477b6e43ad
juanferron <meryjuan2019@gmail.com>
2023-11-20 19:23:39 +0100

    Committing local changes before merging with JavierRocha92/crud

--- clinicaVeterinariaTalavera/pages/crud.php
+++ clinicaVeterinariaTalavera/pages/crud.php
@@ -5,7 +5,7 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <link rel="stylesheet" href="../assets/css/style.css">
-        <title>Página</title>
+        <title>Clinica Veterinaria Talavera</title>
     </head>
     <body>
 

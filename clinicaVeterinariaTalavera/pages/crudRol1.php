324ec67c05b41b3a354c79e0f05bde0741558192
javie <javierrg1992@gmail.com>
2023-11-20 18:00:03 +0100

    user logic interfaz done, update rol 1 user first step

--- clinicaVeterinariaTalavera/pages/crudRol1.php
+++ clinicaVeterinariaTalavera/pages/crudRol1.php
@@ -1,12 +1,29 @@
 <?php
 require '../files/functions.php';
 $user = 'Laura';
+//declaramos una varible para poder controlar cuando un cliente esta bajo una accion del ususario
+$matches = false;
 $bd = connectionBBDD('mysql:dbname=exposicion;host=127.0.0.1', 'root', '');
 //comprobamos si existe usuario desde ela rchivo requerido anterior se maneja esta posibilidad y se guarda en una variable
 if (isset($user)) {
     //rezlimaos la consulta para sacar por pantalla los nombres de todos los usuarios de la veterinaria
-    $sql = "select nombre,apellido,telefono,direccion FROM personas";
+    $sql = "select dni,nombre,apellido,telefono,direccion FROM personas";
     $clients = selectValues($bd, $sql);
+}
+//filtramos el formulario que nos viene por post
+if (filter_input_array(INPUT_POST)) {
+    if (isset($_POST['update'])) {
+        $update = htmlspecialchars($_POST['update']);
+    }
+    if (isset($_POST['delete'])) {
+        $delete = htmlspecialchars($_POST['delete']);
+    }
+    if (isset($_POST['commit'])) {
+        $commit = htmlspecialchars($_POST['commit']);
+        //contruimos una sentencia para modificar los datos del registro de donde se haga refernecia en la variable commmit, para ellos llamamos a una metodo que nos filtra los
+        //resultado y nos constuye una sentencia 
+        $sql = makeStatement('personas', $_POST);
+    }
 }
 //Cerramos la conexión con la base de datos 
 $bd = null;
@@ -34,31 +51,64 @@ $bd = null;
         <div>
             <main class="main">
                 <?php
-                //Controlamos si existe la consulta si a devulto algo para poder controlar los errores
-                if (isset($clients)) {
-                    ?>
-                    <h2>Estas son tus mascotas <?= $user ?></h2>
-                    <table border="1">
-                        <thead>
+                ?>
+                <h2>Estas son tus mascotas <?= $user ?></h2>
+                <table border="1">
+                    <thead>
+                        <tr>
+                            <th class="td">Nombre</th>
+                            <th class="td">Apellidos</th>
+                            <th class="td">Teléfono</th>
+                            <th class="td">Dirección</th>
+                            <th class="td">Mascotas</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        //recorremos al arrya de los registros que de la consulta y creamos un tr para cada mascota
+                        foreach ($clients as $client) {
+                            //Creamos un formulario para guardar los inputs qeu generamos para cada cliente
+                            ?>
+                        <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
                             <tr>
-                                <th class="td">Nombre</th>
-                                <th class="td">Apellidos</th>
-                                <th class="td">Teléfono</th>
-                                <th class="td">Dirección</th>
-                                <th class="td">Mascotas</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            //recorremos al arrya de los registros que de la consulta y creamos un tr para cada mascota
-                            foreach ($clients as $client) {
-                                ?>
-                                <tr>
-                                    <?php
-                                    //recorremos el array para cada registro en este caso para cada mascota
+                                <?php
+                                //recorremos el array para cada registro en este caso para cada mascota
 
-                                    foreach ($client as $key => $value) {
-                                        //creamos un td para imprimir cada uno de los valores de las columnas, dentro metremos un input para hacer el campo editable
+                                foreach ($client as $key => $value) {
+                                    //controlamos con este  if que el dni del cliente no se muestre por pantalla
+                                    if($key != 'dni'){
+                                        
+                                    
+                                    //en cada pasada del bucle controlamos que la varibale de coinicidencia $matches este a false cada vez que se itera un cliente
+                                    $matches = false;
+                                    //creamos un td para imprimir cada uno de los valores de las columnas, dentro metremos un input para hacer el campo editable
+                                    //Controlamos si existe la consulta si a devulto algo para poder controlar los errores,
+                                    if (isset($update)) {
+                                        //HAY QUE ARREGLAR ESTO PORQUE TENEMOS QUE HACER EN UN MISMO IF SI EXISTE EL UPDATE Y SI COINCIDE EL NOMBRE DE ESTE PARA NO GENERAR 
+                                        //LOS INPUT DISABLES DOS VECES, QUIZAS CON UNA TERNARIO
+                                        if ($update === $client['nombre']) {
+                                            //le damos a la varibale matches el valor true para mas abajo generar un input confirmas cambios en lugar de modificar
+                                            $matches = true;
+                                            ?>
+
+                                            <td class="td">
+                                                <input name="<?= $key ?>" value="<?= $value ?>">
+                                            </td>
+
+                                            <?php
+                                        } else {
+                                            //este condicional nos va a controlar que los input se generen disabled para no poder modificarlo
+                                            ?>
+
+                                            <td class="td">
+                                                <input name="<?= $key ?>" disabled value="<?= $value ?>">
+                                            </td>
+
+                                            <?php
+                                        }
+                                    } else {
+//                                                  echo 'algo va mal porque existe el update pero no coincide el nombre del cliente';
+                                        //este condicional nos va a controlar que los input se generen disabled para no poder modificarlo
                                         ?>
 
                                         <td class="td">
@@ -67,54 +117,66 @@ $bd = null;
 
                                         <?php
                                     }
-                                    //Realizamos una conexión a la base de datos para obtener el objeto con el que realizaremos la consulta
-                                    $bd = connectionBBDD('mysql:dbname=exposicion;host=127.0.0.1', 'root', '');
-                                    //generamos una consulta para las masscotas de cada cliente
-                                    //guardo en una variable el nombre del cliente sobre el que se genera la consulta
-                                    $client_name = $client['nombre'];
-                                    $mascotas = selectValues($bd, "SELECT nombre FROM mascotas WHERE dni_propietario in (SELECT dni FROM personas WHERE nombre = '$client_name')");
-                                    //Cerramos la conexión con la base de datos
-                                    $bd = null;
-                                    if (isset($mascotas)) {
-                                        //recoremos el array con los registros de las mascotas de cada cliente
-                                        //creamos un select para guardad todas los nombres de las mascotas en options
-                                        ?>
-                                        <td class="td">
-                                            <select class="select" name="puppies">
-                                                <?php
-                                                foreach ($mascotas as $mascota) {
-                                                    //para cada creamos una etiqueta option y la rellenamos con el nombre de la mascota del cliente
-                                                    ?>
-                                                    <option class="option" name="puppy" value="<?= $mascota['nombre'] ?>"><?= $mascota['nombre'] ?></option>
-                                                    <?php
-                                                }
-                                                //cerramos la etiqueta select
-                                                ?>
-                                            </select>
-                                        </td>
-                                        <?php
-                                        //Creamos dos etiquetas <td> para guardar lso botones de editar o borrar y dentro meteremos un formulario
-                                        ?>
-                                        <td>
-                                            <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
-                                                <button type="submit" name="update" value="<?= $client['nombre'] ?>">Modificar</button>
-                                                <button type="submit" name="delete" value="<?= $client['nombre'] ?>">Eliminar</button>
-                                            </form>
-                                        </td>
-                                        <?php
-                                    }
+                                }
+                                }
+                                //Realizamos una conexión a la base de datos para obtener el objeto con el que realizaremos la consulta
+                                $bd = connectionBBDD('mysql:dbname=exposicion;host=127.0.0.1', 'root', '');
+                                //generamos una consulta para las masscotas de cada cliente
+                                //guardo en una variable el nombre del cliente sobre el que se genera la consulta
+                                $client_name = $client['nombre'];
+                                $mascotas = selectValues($bd, "SELECT nombre FROM mascotas WHERE dni_propietario in (SELECT dni FROM personas WHERE nombre = '$client_name')");
+                                //Cerramos la conexión con la base de datos
+                                $bd = null;
+                                if (isset($mascotas)) {
+                                    //recoremos el array con los registros de las mascotas de cada cliente
+                                    //creamos un select para guardad todas los nombres de las mascotas en options
                                     ?>
-                                </tr>
-                                <?php
-                            }
-                            ?>
-    <!--<td class="td">Hola</th>-->
+                                    <td class="td">
+                                        <select class="select" name="puppies">
+                                            <?php
+                                            foreach ($mascotas as $mascota) {
+                                                //para cada creamos una etiqueta option y la rellenamos con el nombre de la mascota del cliente
+                                                ?>
+                                                <option class="option" name="puppy" value="<?= $mascota['nombre'] ?>"><?= $mascota['nombre'] ?></option>
+                                                <?php
+                                            }
+                                            //cerramos la etiqueta select
+                                            ?>
+                                        </select>
+                                    </td>
+                                    <?php
+                                    //Creamos dos etiquetas <td> para guardar lso botones de editar o borrar y dentro meteremos un formulario
+                                    ?>
+                                    <td>
 
-                        </tbody>
-                    </table>
-                    <?php
-                }//final if
-                ?>
+                                        <?php
+                                        //condicinal para controlar que input se va a generar dependiendo de si se ha encontrado el cliente a modificar
+                                        if ($matches) {
+                                            ?>
+                                        <input name="dni" hidden value="<?= $client['dni'] ?>">
+                                            <button type="submit" name="commit" value="<?= $client['nombre'] ?>">Confirmar</button>
+
+                                            <?php
+                                        } else {
+                                            ?>
+                                            <button type="submit" name="update" value="<?= $client['nombre'] ?>">Modificar</button>
+                                            <?php
+                                        }
+                                        ?>
+                                        <button type="submit" name="delete" value="<?= $client['nombre'] ?>">Eliminar</button>
+                            </form>
+                            </td>
+                            <?php
+                        }
+                        ?>
+                        </tr>
+                        <?php
+                    }
+                    ?>
+                    </tbody>
+                </table>
+                <!--final de la tabla de informacion de las cosnultas--> 
+                <?php ?>
 
             </main>
             <footer class="footer">

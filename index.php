0375d936d590b5cf89cea9ef1f5147bcdb057a1b
juanferron <meryjuan2019@gmail.com>
2023-11-30 17:42:55 +0100

    solutions over merge

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
     <link rel="stylesheet" href="./css/menu.css">
     <link rel="stylesheet" href="./css/service.css">
     <link rel="stylesheet" href="./css/breakpoint.css">
-    <link rel="stylesheet" href="../css/styleBis.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
 </head>
 
